f8b4b3771b73a3caa0c0c32d0c4e3622105c995d
sanjayrabadiya <sanjayrabadiya30@gmail.com>
2023-12-07 00:32:48 +0530

    working home page

--- js/custom.js
+++ js/custom.js
@@ -5,29 +5,59 @@
 $(document).ready(function(){
 	
 	
-	if($('.service-slider').length > 0) {
-		$('.service-slider').slick({
+	if($('.product-slider').length > 0) {
+		$('.product-slider').slick({
 			dots: false,
 			infinite: true,
-			slidesToShow: 3,
+			slidesToShow: 5,
 		    slidesToScroll: 1,
 		    arrows: true,
-		    prevArrow:"<div class='slick-prev'><span class='icon-left-open'></span></div>",
-			nextArrow:"<div class='slick-next'><span class='icon-right-open'></span></div>",
+		    prevArrow:"<div class='slick-prev'></div>",
+			nextArrow:"<div class='slick-next'></div>",
 		    responsive: [
 				{
-				  breakpoint: 991,
-				  settings: {
-					slidesToShow: 2
-				  }
+					breakpoint: 1200,
+					settings: {
+					  	slidesToShow: 4
+					}
 				},
 				{
-				  breakpoint: 575,
-				  settings: {
-					slidesToShow: 1
-				  }
+					breakpoint: 991,
+					settings: {
+						slidesToShow: 3
+					}
+				},
+				{
+					breakpoint: 767,
+					settings: {
+					  	slidesToShow: 2
+					}
+				},
+				{
+					breakpoint: 575,
+					settings: {
+						slidesToShow: 1
+					}
 				}
-			  ]
+			]
+		});
+	}
+	if($('.raves-reviews-slider').length > 0) {
+		$('.raves-reviews-slider').slick({
+			dots: false,
+			infinite: true,
+			slidesToShow: 1,
+		    slidesToScroll: 1,
+		    arrows: true,
+		    prevArrow:"<div class='slick-prev'></div>",
+			nextArrow:"<div class='slick-next'></div>",		    
+		});
+	}
+	
+
+	if($('.tab-nav').length > 0) {
+		$('.tab-nav span').on('click', function() {
+			$([$(this).parent()[0], $($(this).data('href'))[0]]).addClass('active').siblings('.active').removeClass('active');
 		});
 	}
 		
